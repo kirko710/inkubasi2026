aa7a817c9c75121653aa45c9e801678c2b107f52
kirko710 <reyhanhadyan710@student.ub.ac.id>
2026-03-27 11:33:34 +0000

    fix: add PrometheusModule to app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { CartModule } from './cart/cart.module';
 import { AdminModule } from './admin/admin.module';
+import { MetricsInterceptor } from './common/interceptors/metrics.interceptor';
+import {
+  ORDER_CREATED_COUNTER,
+  CART_ITEMS_ADDED_COUNTER,
+  HTTP_REQUEST_DURATION,
+} from './metrics';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    PrometheusModule.register({
+      defaultMetrics: { enabled: true },
+      path: '/metrics',
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => ({
@@ -32,6 +43,12 @@ import { AdminModule } from './admin/admin.module';
     OrdersModule,
     CartModule,
     AdminModule,
+  ],
+  providers: [
+    ORDER_CREATED_COUNTER,
+    CART_ITEMS_ADDED_COUNTER,
+    HTTP_REQUEST_DURATION,
+    MetricsInterceptor,
   ],
 })
 export class AppModule {}
